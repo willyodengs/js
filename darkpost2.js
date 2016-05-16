cc047d041a605084dbdaad02f63182b7c3501b3c
Adhy Suryadi <KompiAjaib@users.noreply.github.com>
2016-05-16 11:35:41 +0700

    Update darkpost2.js
    Merubah resize popular post image dari s1600 menjadi s72-c

--- darkpost2.js
+++ darkpost2.js
@@ -57,7 +57,7 @@ function resizeThumbnail(parentID, size, size2) {
     var parent = document.getElementById(parentID),
         image = parent.getElementsByTagName('img');
     for (var i = 0; i < image.length; i++) {
-        image[i].src = image[i].src.replace(/\/s1600/, "/s" + size + "");
+        image[i].src = image[i].src.replace(/\/s72\-c/, "/s" + size + "");
         image[i].width = size;
         image[i].height = size2;
     }
