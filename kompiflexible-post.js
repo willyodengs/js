6ba2824bfdd5554b22fef2f158f3c73c865ba31f
Adhy Suryadi <KompiAjaib@users.noreply.github.com>
2016-05-18 12:47:22 +0700

    Add files via upload

---
+++ kompiflexible-post.js
@@ -0,0 +1,73 @@
+var disqus_url = disqus_blogger_current_url;
+
+(function () {
+    "use strict";
+    var get_comment_block = function () {
+        var block = document.getElementById('comments');
+        if (!block) {
+            block = document.getElementById('disqus-blogger-comment-block');
+        }
+        return block;
+    };
+    var comment_block = get_comment_block();
+    if (!!comment_block) {
+        var disqus_div = document.createElement('div');
+        disqus_div.id = 'disqus_thread';
+        comment_block.innerHTML = '';
+        comment_block.appendChild(disqus_div);
+        comment_block.style.display = 'block';
+        var dsq = document.createElement('script');
+        dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.body).appendChild(dsq);
+    }
+})();
+
+function resizeThumb(e,t){for(var s=document.getElementById(e),r=s.getElementsByTagName("img"),c=0;c<r.length;c++)r[c].src=r[c].src.replace(/\/s512\-c/,"/s"+t),r[c].width=t,r[c].height=t}resizeThumb("authorphoto",36);
+
+!function(e,n,t){var o,c=e.getElementsByTagName(n)[0];e.getElementById(t)||(o=e.createElement(n),o.id=t,o.src="//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3",c.parentNode.insertBefore(o,c))}(document,"script","facebook-jssdk");
+
+    var divs = ["disqus-box", "blogger-box", "facebook-box"];
+    var visibleDivId = null;
+    function toggleVisibility(divId) {
+      if(visibleDivId === divId) {
+        visibleDivId = null;
+      } else {
+        visibleDivId = divId;
+      }
+      hideNonVisibleDivs();
+    }
+    function hideNonVisibleDivs() {
+      var i, divId, div;
+      for(i = 0; i < divs.length; i++) {
+        divId = divs[i];
+        div = document.getElementById(divId);
+        if(visibleDivId === divId) {
+          div.style.display = "block";
+        } else {
+          div.style.display = "none";
+        }
+      }
+    }
+
+$(".commentbtn").click(function (e) {
+$(this).addClass("btncurrent").siblings().removeClass("btncurrent");
+});
+
+  (function() {
+    var dr = document.createElement('script');
+    dr.type = 'text/javascript'; dr.async = true;
+    dr.src = '//share.donreach.com/buttons.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dr);
+  })();
+
+(function($){   
+    var newerLink = $('a.blog-pager-newer-link');
+    var olderLink = $('a.blog-pager-older-link');
+    $.get(newerLink.attr('href'), function (data) {
+     newerLink.html($(data).find('.post h1.post-title').text());   
+    },"html");
+    $.get(olderLink.attr('href'), function (data2) {
+     olderLink.html($(data2).find('.post h1.post-title').text());   
+    },"html");
+})(jQuery);
