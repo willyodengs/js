339be5865508837ab2843471369d3a22c4c8377b
Adhy Suryadi <KompiAjaib@users.noreply.github.com>
2016-12-06 18:19:31 +0700

    Add files via upload

---
+++ balloon-addthis.js
@@ -0,0 +1,5 @@
+(function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-567cc9fc12a84127';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
