c3d517ce1f303ec147edec01b05aadb56398f786
Adhy Suryadi <KompiAjaib@users.noreply.github.com>
2017-06-01 10:00:26 +0700

    Add files via upload

---
+++ olshop_review3.js
@@ -0,0 +1,9 @@
+(function() {
+    if ('WIDGETPACK_LOADED' in window) return;
+    WIDGETPACK_LOADED = true;
+    var mc = document.createElement('script');
+    mc.type = 'text/javascript';
+    mc.async = true;
+    mc.src = 'https://embed.widgetpack.com/widget.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(mc, s.nextSibling);
+})();
