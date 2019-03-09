1c82627de2bf2cfee76d475051e0dc57df77ac0f
Adhy Suryadi <KompiAjaib@users.noreply.github.com>
2019-03-09 11:37:25 +0700

    Create ayoviralkan_home.js

---
+++ ayoviralkan_home.js
@@ -0,0 +1,56 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("myTopnav").style.left = "0";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("myTopnav").style.left = "-250px";
+}
+
+// Add active class to the current button (highlight it)
+var btns = document.getElementsByClassName("dropbtn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {  
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) { 
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+  });
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropbtn");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('active')) {
+        openDropdown.classList.remove('active');
+      }
+    }
+  }
+}
+function menuoverlayOn() {
+    document.getElementById("menuoverlay").style.display = "block";
+}
+
+function menuoverlayOff() {
+    document.getElementById("menuoverlay").style.display = "none";
+}
+
+function addClassBody() {
+    var element, name, arr;
+    element = document.body;
+    name = "flow";
+    arr = element.className.split(" ");
+    if (arr.indexOf(name) == -1) {
+        element.className += " " + name;
+    }
+}
+function removeClassBody() {
+    var element = document.body;
+    element.className = element.className.replace(/\bflow\b/g, "");
+}
